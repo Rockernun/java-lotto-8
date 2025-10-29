73387d50cfad43d14c7bca07745c18e67a9cab84
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-29 11:07:44 +0900

    feat(Lotto): Lotto 구현
    - 번호가 6개인지 검증
    - 번호 범위가 1부터 45 사이인지 검증
    - 중복된 숫자가 있는지 검증
    - 모든 예외 처리 메시지 형식에 [ERROR] 추가(요구 사항)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,40 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateNumberCount(numbers);
+        validateNumberInRange(numbers);
+        validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateNumberCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumberInRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < 0 || number > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!set.add(number)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안 됩니다.");
+            }
+        }
+    }
+
 }
