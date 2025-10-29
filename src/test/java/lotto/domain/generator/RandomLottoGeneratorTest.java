41ebb4e2db078ade602cb805175d7b27e851aaa3
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-29 12:40:49 +0900

    test(generator): LottoGenerator 범위 검증 단위 테스트 추가

---
+++ src/test/java/lotto/domain/generator/RandomLottoGeneratorTest.java
@@ -0,0 +1,19 @@
+package lotto.domain.generator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RandomLottoGeneratorTest {
+
+    @DisplayName("범위 내에서 숫자가 생성되는지 검증한다.")
+    @Test
+    void 범위_내에서_숫자가_생성되는지_검증한다() {
+        LottoGenerator generator = new RandomLottoGenerator();
+        for (int i = 0; i < 1000; i++) {
+            int number = generator.generate();
+            assertThat(number).isBetween(1, 45);
+        }
+    }
+}
