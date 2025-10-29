a88fa70dc8a05cbad8b87552557f13578ed4570d
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-29 17:04:35 +0900

    test(Rank): 번호 일치 여부에 따른 등수 단위 테스트 추가
    - 번호 6개 일치: 1등
    - 번호 5개 일치 & 보너스 번호 일치: 2등
    - 번호 5개 일치: 3등
    - 번호 4개 일치: 4등
    - 번호 3개 일치: 5등
    - 그 외는 등수 없음

---
+++ src/test/java/lotto/domain/model/RankTest.java
@@ -0,0 +1,38 @@
+package lotto.domain.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class RankTest {
+
+    @Test
+    void 번호_6개가_일치할_경우_1등인지_검증한다() {
+        assertThat(Rank.of(6, false)).isEqualTo(Rank.FIRST);
+    }
+
+    @Test
+    void 번호_5개가_일치하고_보너스_번호도_일치하는_경우_2등인지_검증한다() {
+        assertThat(Rank.of(5, true)).isEqualTo(Rank.SECOND);
+    }
+
+    @Test
+    void 번호_5개가_일치할_경우_3등인지_검증한다() {
+        assertThat(Rank.of(5, false)).isEqualTo(Rank.THIRD);
+    }
+
+    @Test
+    void 번호_4개가_일치할_경우_4등인지_검증한다() {
+        assertThat(Rank.of(4, false)).isEqualTo(Rank.FOURTH);
+    }
+
+    @Test
+    void 번호_3개가_일치할_경우_5등인지_검증한다() {
+        assertThat(Rank.of(3, false)).isEqualTo(Rank.FIFTH);
+    }
+
+    @Test
+    void 그_외의_경우를_검증한다() {
+        assertThat(Rank.of(1, false)).isEqualTo(Rank.OTHERS);
+    }
+}
