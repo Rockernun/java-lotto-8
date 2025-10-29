2c02c49b089b15d116723ec3b80bc576f245b68f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-29 15:02:51 +0900

    feat(parser): Parser 인터페이스 및 CommaParser 구현체 구현
    - 쉼표 이외의 다른 구분자로 입력을 파싱해야 할 경우에 대비해서 Parser로 추상화 처리

---
+++ src/main/java/lotto/domain/parser/CommaParser.java
@@ -0,0 +1,24 @@
+package lotto.domain.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommaParser implements Parser {
+
+    private static final String DELIMITER = ",";
+
+    @Override
+    public List<Integer> parse(String input) {
+        List<Integer> result = new ArrayList<>();
+        for (String s : input.split(DELIMITER)) {
+            String trim = s.trim();
+            if (trim.isEmpty()) {
+                throw new IllegalArgumentException("[ERROR] 쉼표 사이에 공백은 허용되지 않습니다.");
+            }
+
+            result.add(Integer.parseInt(trim));
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/lotto/domain/parser/Parser.java
@@ -0,0 +1,7 @@
+package lotto.domain.parser;
+
+import java.util.List;
+
+public interface Parser {
+    List<Integer> parse(String input);
+}
