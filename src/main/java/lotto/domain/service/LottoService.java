688ae71c8d93a2840c520eee48fb3aea5ab015f6
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-31 21:37:12 +0900

    feat(LottoService): 서비스 구현 및 당첨 집계 로직 추가
    - countMatches(): 로또 번호와 당첨 번호(보너스 번호 포함)를 바탕으로 몇 개의 번호가 일치했는지 계산
    - containsBonusNumber(): 로또 번호에 보너스 번호가 포함되어 있는지 확인
    - initLottoList(): 구매한 금액만큼의 로또를 생성
    - evaluate(): countMatches() 메서드와 containsBonusNumber() 메서드로 판단한 당첨 결과의 등수와 횟수를 반환

---
+++ src/main/java/lotto/domain/service/LottoService.java
@@ -0,0 +1,64 @@
+package lotto.domain.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.model.Lotto;
+import lotto.domain.model.Purchase;
+import lotto.domain.model.Rank;
+import lotto.domain.model.Result;
+import lotto.domain.model.WinningNumbers;
+
+public class LottoService {
+
+    private final LottoGenerator lottoGenerator;
+
+    public LottoService(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public List<Lotto> initLottoList(Purchase payment) {
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < payment.getPurchasedLottoCount(); i++) {
+            lottoList.add(Lotto.of(lottoGenerator.generate()));
+        }
+
+        return lottoList;
+    }
+
+    public Result evaluate(List<Lotto> lottoList, WinningNumbers winningNumbers) {
+        Map<Rank, Integer> counts = new HashMap<>();
+        for (Lotto lotto : lottoList) {
+            int matchCount = countMatches(lotto, winningNumbers);
+            boolean hasBonusNumber = containsBonusNumber(lotto, winningNumbers);
+
+            Rank rank = Rank.of(matchCount, hasBonusNumber);
+            counts.put(rank, counts.getOrDefault(rank, 0) + 1);
+        }
+
+        return new Result(counts);
+    }
+
+    private int countMatches(Lotto lotto, WinningNumbers winningNumbers) {
+        int count = 0;
+        for (Integer number : lotto.getLottoNumbers()) {
+            if (winningNumbers.getWinningNumbers().contains(number)) count++;
+            if (number == winningNumbers.getBonusNumber()) count++;
+        }
+
+        return count;
+    }
+
+    private boolean containsBonusNumber(Lotto lotto, WinningNumbers winningNumbers) {
+        boolean containsBonusNumber = false;
+        for (Integer number : lotto.getLottoNumbers()) {
+            if (number == winningNumbers.getBonusNumber()) {
+                containsBonusNumber = true;
+            }
+        }
+
+        return containsBonusNumber;
+    }
+}
