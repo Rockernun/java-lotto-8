5bfd1502c5bfd982027ee2a62fd7ac9994c841ed
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-31 22:13:44 +0900

    feat(Application): main() 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.generator.RandomLottoGenerator;
+import lotto.domain.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoGenerator generator = new RandomLottoGenerator();
+        LottoService lottoService = new LottoService(generator);
+
+        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+        lottoController.run();
     }
 }
