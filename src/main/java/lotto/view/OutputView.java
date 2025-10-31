dd3559c92d6287f47043bc11b145de8c30c38007
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-31 22:00:44 +0900

    feat(OutputView): 출력 뷰 생성
    - printPurchased(): 구매한 로또 개수와 각 로또들을 출력
    - printResult(): 당첨 결과(Result) 모델로부터 데이터를 받아 당첨 통계 출력(코드가 다소 난잡해보임, 추후 개선)
    - printError(): 에러 발생 시 "[ERROR]" 출력
    - getFormat(): 우승 상금 출력 시, 천 단위로 포맷팅 처리

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,36 @@
+package lotto.view;
+
+import java.util.List;
+import lotto.domain.model.Lotto;
+import lotto.domain.model.Purchase;
+import lotto.domain.model.Rank;
+import lotto.domain.model.Result;
+
+public class OutputView {
+
+    public void printPurchased(List<Lotto> lottoList, Purchase purchase) {
+        System.out.printf("%d개를 구매했습니다.\n", purchase.getPurchasedLottoCount());
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto.getLottoNumbers());
+        }
+    }
+
+    public void printResult(Result result, int payment) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.printf("%d개 일치 (%s원) - %d개\n", Rank.FIRST.getMatchedNumberCount(), getFormat(Rank.FIRST), result.count(Rank.FIRST));
+        System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n", Rank.SECOND.getMatchedNumberCount(), getFormat(Rank.SECOND), result.count(Rank.SECOND));
+        System.out.printf("%d개 일치 (%s원) - %d개\n", Rank.THIRD.getMatchedNumberCount(), getFormat(Rank.THIRD), result.count(Rank.THIRD));
+        System.out.printf("%d개 일치 (%s원) - %d개\n", Rank.FOURTH.getMatchedNumberCount(), getFormat(Rank.FOURTH), result.count(Rank.FOURTH));
+        System.out.printf("%d개 일치 (%s원) - %d개\n", Rank.FIFTH.getMatchedNumberCount(), getFormat(Rank.FIFTH), result.count(Rank.FIFTH));
+        System.out.printf("총 수익률은 %s입니다.\n", result.yieldRate(payment));
+    }
+
+    public void printError(String message) {
+        System.out.println(message);
+    }
+
+    private static String getFormat(Rank rank) {
+        return String.format("%,d", rank.getPrize());
+    }
+}
