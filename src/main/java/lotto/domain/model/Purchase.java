b883eaab4cb13969edf822e1dd992ae16740dca5
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-01 01:50:03 +0900

    refactor(Purchase): 지불 금액 필드 추가 및 생성자 추가
    - if 블록 중괄호 제거
    - purchaseAmount를 payment로 변경

--- src/main/java/lotto/domain/model/Purchase.java
+++ src/main/java/lotto/domain/model/Purchase.java
@@ -2,31 +2,30 @@ package lotto.domain.model;
 
 public class Purchase {
 
-    private final int purchasedLottoCount;
     private static final int PAYMENT_UNIT = 1000;
 
-    public Purchase(int purchaseAmount) {
-        validatePaymentIsPositive(purchaseAmount);
-        validatePaymentIsMultipleOfPrice(purchaseAmount);
-        this.purchasedLottoCount = calculateLottoCount(purchaseAmount);
+    private final int payment;
+
+    public Purchase(int payment) {
+        validatePaymentIsPositive(payment);
+        validatePaymentIsMultipleOfPrice(payment);
+        this.payment = payment;
+    }
+
+    public int getPayment() {
+        return payment;
     }
 
     public int getPurchasedLottoCount() {
-        return purchasedLottoCount;
+        return payment / PAYMENT_UNIT;
     }
 
-    private int calculateLottoCount(int purchaseAmount) {
-        return purchaseAmount / PAYMENT_UNIT;
+    private void validatePaymentIsPositive(int payment) {
+        if (payment <= 0) throw new IllegalArgumentException("[ERROR] 구매 금액은 반드시 양수여야 합니다.");
     }
 
-    private void validatePaymentIsPositive(int purchaseAmount) {
-        if (purchaseAmount <= 0) {
-            throw new IllegalArgumentException("[ERROR] 구매 금액은 반드시 양수여야 합니다.");
-        }
-    }
-
-    private void validatePaymentIsMultipleOfPrice(int purchaseAmount) {
-        if (purchaseAmount % PAYMENT_UNIT != 0) {
+    private void validatePaymentIsMultipleOfPrice(int payment) {
+        if (payment % PAYMENT_UNIT != 0) {
             throw new IllegalArgumentException(String.format("[ERROR] 구매 금액은 %d원 단위여야 합니다.", PAYMENT_UNIT));
         }
     }
