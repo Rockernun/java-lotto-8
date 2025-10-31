bd1e1da53e0df179cbb9b98a3636c4995f9e1502
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-31 21:56:52 +0900

    refactor(InputView): 순수 입력과 관련된 검증 로직 추가
    - 도메인과는 관련없는 "입력이 비어 있음", "숫자만 허용" 과 같은 검증 로직은 뷰에서 처리하도록 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,21 +1,78 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.domain.parser.CommaParser;
+import lotto.domain.parser.Parser;
 
 public class InputView {
 
+    public static final String REGEX = "^[0-9]*$";
+
     public String inputPayment() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        validatePaymentIsNumber(input);
+        validatePaymentIsEmpty(input);
+
+        return input;
     }
 
     public String inputWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        Parser parser = new CommaParser();
+        List<Integer> parsedNumber = parser.parse(input);
+        validateWinningNumbersAreEmpty(input);
+        for (Integer number : parsedNumber) {
+            validateWinningNumbersAreNumber(String.valueOf(number));
+        }
+
+        return input;
     }
 
     public String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
-        return Console.readLine();
+        String input = Console.readLine();
+        validateBonusNumberIsEmpty(input);
+        validateBonusNumberIsNumber(input);
+
+        return input;
+    }
+
+    private void validatePaymentIsEmpty(String input) {
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액이 비어 있습니다.");
+        }
+    }
+
+    private void validatePaymentIsNumber(String input) {
+        if (!input.matches(REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private void validateWinningNumbersAreEmpty(String input) {
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호가 비어 있습니다.");
+        }
+    }
+
+    private void validateWinningNumbersAreNumber(String input) {
+        if (!input.matches(REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 입력 가능합니다.");
+        }
+    }
+
+    private void validateBonusNumberIsEmpty(String input) {
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 비어 있습니다.");
+        }
+    }
+
+    private void validateBonusNumberIsNumber(String input) {
+        if (!input.matches(REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자만 입력 가능합니다.");
+        }
     }
 }
