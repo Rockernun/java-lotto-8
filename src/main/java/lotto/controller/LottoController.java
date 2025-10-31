ba6444f2988a8759c6a2a8b1c838e3f20ee5e961
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-31 22:10:02 +0900

    feat(LottoController): 컨트롤러 구현
    - run(): 구매 금액을 입력 받아 서비스 로직에 전달하고, 생성된 로또 리스트를 출력하도록 함. 추가로, 당첨 번호를 입력 받아 서비스 로직에 전달하고, 반환 받은 당첨 결과를 뷰로 출력하도록 처리
    - readPurchase(): 구매 금액 입력 시, 예외가 발생하면 재입력하도록 처리
    - readWinningNumbers(): 당첨 번호와 보너스 번호 입력 시, 예외가 발생하면 재입력하도록 처리

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,59 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.domain.model.Lotto;
+import lotto.domain.model.Purchase;
+import lotto.domain.model.Result;
+import lotto.domain.model.WinningNumbers;
+import lotto.domain.parser.CommaParser;
+import lotto.domain.parser.Parser;
+import lotto.domain.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoService lottoService;
+
+    public LottoController(InputView inputView, OutputView outputView,LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoService = lottoService;
+    }
+
+    public void run() {
+        Purchase purchase = readPurchase();
+        List<Lotto> lottoList = lottoService.initLottoList(purchase);
+        outputView.printPurchased(lottoList, purchase);
+
+        WinningNumbers winningNumbers = readWinningNumbers();
+        Result result = lottoService.evaluate(lottoList, winningNumbers);
+        outputView.printResult(result, purchase.getPayment());
+    }
+
+    private Purchase readPurchase() {
+        while (true) {
+            try {
+                String payment = inputView.inputPayment();
+                return new Purchase(Integer.parseInt(payment.trim()));
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    private WinningNumbers readWinningNumbers() {
+        Parser parser = new CommaParser();
+        while (true) {
+            try {
+                String winningNumbers = inputView.inputWinningNumbers();
+                String bonusNumber = inputView.inputBonusNumber();
+                return WinningNumbers.of(parser.parse(winningNumbers), Integer.parseInt(bonusNumber.trim()));
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+}
