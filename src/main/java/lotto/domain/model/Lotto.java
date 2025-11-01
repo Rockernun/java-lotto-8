1c067d8e97c30c6af19cbae1310541cb4e9befe9
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-01 20:32:30 +0900

    refactor(Lotto): 번호의 범위, 번호의 개수, 중복 여부 검증 로직 대체

--- src/main/java/lotto/domain/model/Lotto.java
+++ src/main/java/lotto/domain/model/Lotto.java
@@ -1,21 +1,13 @@
 package lotto.domain.model;
 
-import static lotto.domain.LottoRules.lottoSize;
-import static lotto.domain.LottoRules.maxNumber;
-import static lotto.domain.LottoRules.minNumber;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
 
     private final List<Integer> numbers;
 
     private Lotto(List<Integer> numbers) {
-        validateNumberCount(numbers);
-        validateNumberInRange(numbers);
-        validateDuplicateNumbers(numbers);
+        LottoNumbers.validateNumbers(numbers);
         this.numbers = numbers;
     }
 
@@ -25,28 +17,5 @@ public class Lotto {
 
     public List<Integer> getLottoNumbers() {
         return numbers;
-    }
-
-    private void validateNumberCount(List<Integer> numbers) {
-        if (numbers.size() != lottoSize()) {
-            throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d개 입니다.", lottoSize()));
-        }
-    }
-
-    private void validateNumberInRange(List<Integer> numbers) {
-        for (Integer number : numbers) {
-            if (number < minNumber() || number > maxNumber()) {
-                throw new IllegalArgumentException(String.format("[ERROR] 로또 번호는 %d부터 %d사이의 숫자여야 합니다.", minNumber(), maxNumber()));
-            }
-        }
-    }
-
-    private void validateDuplicateNumbers(List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>();
-        for (Integer number : numbers) {
-            if (!set.add(number)) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안 됩니다.");
-            }
-        }
     }
 }
