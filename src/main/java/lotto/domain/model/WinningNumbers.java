ecbc5d15fb9f4b6408ecd8057f89240f23c731e4
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-01 20:32:58 +0900

    refactor(WinningNumbers): 번호의 범위, 번호의 개수, 중복 여부 검증 로직 대체
    - 설정된 범위에서 이미 양수임이 보장되기 때문에 양수 검증 로직 삭제

--- src/main/java/lotto/domain/model/WinningNumbers.java
+++ src/main/java/lotto/domain/model/WinningNumbers.java
@@ -1,12 +1,9 @@
 package lotto.domain.model;
 
-import static lotto.domain.LottoRules.lottoSize;
-import static lotto.domain.LottoRules.maxNumber;
-import static lotto.domain.LottoRules.minNumber;
+import static lotto.domain.model.LottoRules.maxNumber;
+import static lotto.domain.model.LottoRules.minNumber;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class WinningNumbers {
 
@@ -16,11 +13,7 @@ public class WinningNumbers {
     private WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
-        validateWinningNumberIsPositive(winningNumbers);
-        validateWinningNumberInRange(winningNumbers);
-        validateNumberCount(winningNumbers);
-        validateDuplicateWinningNumber(winningNumbers);
-        validateBonusNumberIsPositive(bonusNumber);
+        LottoNumbers.validateNumbers(winningNumbers);
         validateBonusNumberInRange(bonusNumber);
         validateDuplicateBonusNumber(bonusNumber);
     }
@@ -35,35 +28,6 @@ public class WinningNumbers {
 
     public int getBonusNumber() {
         return bonusNumber;
-    }
-
-    private void validateWinningNumberIsPositive(List<Integer> parsedNumbers) {
-        for (Integer number : parsedNumbers) {
-            if (number <= 0) throw new IllegalArgumentException("[ERROR] 당첨 번호는 양수여야 합니다.");
-        }
-    }
-
-    private void validateWinningNumberInRange(List<Integer> parsedNumbers) {
-        for (Integer number : parsedNumbers) {
-            if (number < minNumber() || number > maxNumber()) throw new IllegalArgumentException("[ERROR] 당첨 번호가 범위를 벗어났습니다.");
-        }
-    }
-
-    private void validateNumberCount(List<Integer> parsedNumbers) {
-        if (parsedNumbers.size() != lottoSize()) throw new IllegalArgumentException(String.format("[ERROR] 당첨 번호의 개수는 %d개여야 합니다.", lottoSize()));
-    }
-
-    private void validateDuplicateWinningNumber(List<Integer> parsedNumbers) {
-        Set<Integer> set = new HashSet<>();
-        for (Integer number : parsedNumbers) {
-            if (!set.add(number)) {
-                throw new IllegalArgumentException("[ERROR] 당첨 번호가 중복되었습니다.");
-            }
-        }
-    }
-
-    private void validateBonusNumberIsPositive(int bonusNumber) {
-        if (bonusNumber <= 0) throw new IllegalArgumentException("[ERROR] 보너스 번호는 양수여야 합니다.");
     }
 
     private void validateBonusNumberInRange(int bonusNumber) {
