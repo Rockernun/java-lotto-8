1ac56341b3903c6a0b7b1ae7078e13512c491ae0
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-01 09:23:12 +0900

    test(generator): RandomLottoGenerator 단위 테스트 추가
    - 6개의 난수가 정상적으로 생성되는지 검증
    - 생성된 난수들이 중복되지 않았는지 검증

--- src/test/java/lotto/domain/generator/RandomLottoGeneratorTest.java
+++ src/test/java/lotto/domain/generator/RandomLottoGeneratorTest.java
@@ -8,15 +8,32 @@ import org.junit.jupiter.api.Test;
 
 class RandomLottoGeneratorTest {
 
+    LottoGenerator generator = new RandomLottoGenerator();
+    List<Integer> generatedNumbers = generator.generate();
+
     @DisplayName("범위 내에서 숫자가 생성되는지 검증한다.")
     @Test
     void 범위_내에서_숫자가_생성되는지_검증한다() {
-        LottoGenerator generator = new RandomLottoGenerator();
         for (int i = 0; i < 1000; i++) {
-            List<Integer> generatedNumbers = generator.generate();
             for (Integer generatedNumber : generatedNumbers) {
                 assertThat(generatedNumber).isBetween(1, 45);
             }
+        }
+    }
+
+    @DisplayName("설정한 개수만큼 난수가 생성되는지 검증한다.")
+    @Test
+    void 설정한_개수만큼_난수가_생성되는지_검증한다() {
+        for (int i = 0; i < 1000; i++) {
+            assertThat(generatedNumbers).hasSize(6);
+        }
+    }
+
+    @DisplayName("생성된 난수들이 중복되는지 검증한다.")
+    @Test
+    void 생성된_난수들이_중복되는지_검증한다() {
+        for (int i = 0; i < 1000; i++) {
+            assertThat(generatedNumbers).doesNotHaveDuplicates();
         }
     }
 }
