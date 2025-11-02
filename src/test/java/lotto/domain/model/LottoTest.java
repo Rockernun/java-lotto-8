3cde23747f749e98b1817097a461e45eea42f100
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-02 20:04:36 +0900

    test(LottoTest): 오름차순 단위 테스트 추가
    - 로또 번호가 정상적으로 오름차순으로 정렬되어 있는지 검증하는 테스트 코드를 추가

--- src/test/java/lotto/domain/model/LottoTest.java
+++ src/test/java/lotto/domain/model/LottoTest.java
@@ -1,10 +1,13 @@
 package lotto.domain.model;
 
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.generator.RandomLottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -28,6 +31,14 @@ class LottoTest {
     void 로또_번호가_범위_내의_값이_아니라면_예외가_발생한다() {
         assertThatThrownBy(() -> Lotto.of(List.of(-1, 0, 5, 47, 97, 101)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 오름차순으로 정렬되어 있는지 검증한다.")
+    @Test
+    void 로또_번호가_오름차순으로_정렬되어_있는지_검증한다() {
+        LottoGenerator generator = new RandomLottoGenerator();
+        Lotto lotto = Lotto.of(generator.generate());
+        assertThat(lotto.getLottoNumbers()).isSorted();
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
