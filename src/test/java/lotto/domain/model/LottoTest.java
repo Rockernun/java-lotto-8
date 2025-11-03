4bcba086dbf933784e434459b7560e93d8dfd360
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-03 11:58:33 +0900

    test(LottoNumbersValidator): 로또 번호에 대한 테스트 이동
    - 기존 Lotto 클래스에 대한 테스트 코드 중 로또 번호에 대한 테스트 로직을 LottoNumbersValidatorTest로 이동
    - 별도의 클래스로 로또의 공통 규칙을 정의한 만큼, 테스트도 여기서 수행해야 하는 것이 적절하다고 판단

--- src/test/java/lotto/domain/model/LottoTest.java
+++ src/test/java/lotto/domain/model/LottoTest.java
@@ -5,33 +5,9 @@ import lotto.domain.generator.RandomLottoGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
-        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("로또 번호의 개수가 6개보다 적으면 예외가 발생한다.")
-    @Test
-    void 로또_번호의_개수가_6개보다_적으면_예외가_발생한다() {
-        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 5)))
-        .isInstanceOf(IllegalArgumentException.class);
-    }
-
-
-    @DisplayName("로또 번호가 범위 내의 값이 아니라면 예외가 발생한다.")
-    @Test
-    void 로또_번호가_범위_내의_값이_아니라면_예외가_발생한다() {
-        assertThatThrownBy(() -> Lotto.of(List.of(-1, 0, 5, 47, 97, 101)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 
     @DisplayName("로또 번호가 오름차순으로 정렬되어 있는지 검증한다.")
     @Test
@@ -39,12 +15,5 @@ class LottoTest {
         LottoGenerator generator = new RandomLottoGenerator();
         Lotto lotto = Lotto.of(generator.generate());
         assertThat(lotto.getLottoNumbers()).isSorted();
-    }
-
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
-        assertThatThrownBy(() -> Lotto.of(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }
